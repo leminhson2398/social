4170c9b376200f2fa3415d5d0d54fcc5ac7f35cc
leminhson <leminhson2398@outlook.com>
2019-05-14 20:18:23 +0700

    rename auth folder in front folder

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,11 @@
 import React, { Fragment } from 'react'
-// import Signing from './components/Auth/Sign'
-// import ProductCard from './components/product/Card'
+import Signing from './components/auth/Sign'
 import AuthUser from './components/GetdataTest'
 
 export default class App extends React.Component {
   render() {
     return (
       <Fragment>
-        {/* <Signing /> */}
-        {/* <ProductCard /> */}
         <AuthUser />
       </Fragment>
     )
